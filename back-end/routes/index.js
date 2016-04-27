a730753b2f3774d2123eeb3d9a07f9d93d6f408c
Robert Bunch <rob@digitalcrafts.com>
2016-04-27 13:35:44 -0400

    Update the search to include and search an array of names. Fill out the post request in Angular

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/search', function(req, res, next) {
+	var name = req.body.name;
+
+	var dcClass = [
+		'Tristan',
+		'Josh',
+		'Bogdan',
+		'Keith',
+		'Will',
+		'Curtis',
+		'Joe',
+		'Kochan',
+		'Patrick',
+		'Jonathan',
+		'Jeremy'
+	];
+
+	if(dcClass.indexOf(name) > -1){
+		res.json({message: "Hello, " + name + ", you are a student in this class."});
+	}else{
+		res.json({message: "You are not in this class."});
+	}
+
 });
+
 
 module.exports = router;
--- front-end/controller.js
+++ front-end/controller.js
@@ -3,7 +3,25 @@ var searchApp = angular.module('searchApp', []);
 searchApp.controller('searchController', function($scope, $http){
 
 	$scope.search = function(){
-		$scope.message = "Hello, World!"
+		$scope.message = "Hello, World!";
+
+		var apiUrl = 'http://localhost:3000/search';
+		
+		$http({
+            method: 'POST',
+            url: apiUrl,
+            data: {name: $scope.who}
+        }).then(
+			function successCallback(response) {
+				$scope.result = response.data.message;
+    			// this callback will be called asynchronously
+    			// when the response is available
+  			}, function errorCallback(response) {
+  				$scope.result = "ERROR!!!"
+    			// called asynchronously if an error occurs
+    			// or server returns response with an error status.
+  			});
 	}
 
 });
+
